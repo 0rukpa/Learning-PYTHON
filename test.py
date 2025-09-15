defbb451d204d1c76e2bd583c890584bb64d7725
Light Orukpa Diamonds <lightdiamonds401@gmail.com>
2025-09-15 15:03:53 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+
+print("wELCOME TO MY FIRST PROGRAM NIGGA")
+print("The developer is a genius named") 
+name= 'Light'
+print(name);
+print("Not just light, no no, the full name is ")
+name2= 'Light Orukpa Diamonds'
+print(name2)
+
+print("Let us handle soeme basic calculations")
+a=1
+b=2
+c=3
+average = (a+b+c)/3
+print("The average sum of the values, a,b,c, is  "), a+b+c
+print("The average is: ", average)
+
+print("Handling Mathematical Complexities are fun, well, up until you have to deal with more complexities and more unique ones at that.")
